d033c60a9593b61d6a9c1e66e61a255f90df93c3
Amy Hogue <hogue.a@gmail.com>
2018-06-18 12:24:12 -0500

    Add event_type to Events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180618170644) do
+ActiveRecord::Schema.define(version: 20180618172250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180618170644) do
     t.string "schedule"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "event_type"
   end
 
   create_table "prayer_requests", force: :cascade do |t|
