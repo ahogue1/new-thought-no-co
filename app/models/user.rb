377a60af1a94fb26c078329c5d5bf4abea954553
Amy Hogue <hogue.a@gmail.com>
2018-06-18 13:46:05 -0500

    Only allow Admin Users to access admin panel

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :prayer_requests
   has_many :registrations
+
+  def admin?
+    role == "admin"
+  end
 end
