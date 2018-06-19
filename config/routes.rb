5719497d93984d41ef7aaaa4a2cb2209356776d6
Amy Hogue <hogue.a@gmail.com>
2018-06-19 14:10:56 -0500

    Create Events controller and view and link in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :events, only: [:index]
 end
