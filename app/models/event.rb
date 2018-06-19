fcae8ec43915270b6ae605da88b815496adf909e
Amy Hogue <hogue.a@gmail.com>
2018-06-19 12:54:01 -0500

    Customize fields shown in rails_admin for Event and User

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,10 @@ class Event < ApplicationRecord
   monetize :general_price_cents
   monetize :member_price_cents
   monetize :tithe_price_cents
+
+  rails_admin do
+    list do
+      include_fields :id, :title, :start_date, :facilitator, :event_type
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,20 @@ class User < ApplicationRecord
   def admin?
     role == "admin"
   end
+
+  rails_admin do
+    exclude_fields(
+      :reset_password_sent_at,
+      :remember_created_at,
+      :sign_in_count,
+      :current_sign_in_ip,
+      :created_at,
+      :last_sign_in_ip,
+      :current_sign_in_at,
+      :last_sign_in_at,
+      :updated_at,
+      :prayer_requests,
+      :registrations
+    )
+  end
 end
