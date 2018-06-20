5dbcb46f5f603ad69bc92b98cbfba631a2977ec3
Amy Hogue <hogue.a@gmail.com>
2018-06-20 18:17:25 -0500

    Create Group index page and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
-  
+
   resources :events, only: [:index]
+  resources :groups, only: [:index]
   resources :prayer_requests, only: [:new, :create, :edit]
 end
