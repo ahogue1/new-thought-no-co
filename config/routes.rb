57d1536da368c08161ca2284d173f63c6d744d7d
Amy Hogue <hogue.a@gmail.com>
2018-06-20 18:22:45 -0500

    Create Youth page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
+  get 'youth', to: 'pages#youth'
 
   resources :events, only: [:index]
   resources :groups, only: [:index]
