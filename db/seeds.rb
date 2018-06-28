44a51de19ab292d164c05aa91666a26338f8739b
Amy Hogue <hogue.a@gmail.com>
2018-06-28 11:31:55 -0500

    Style footer and change navbar links to match requested items

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,7 +52,7 @@ event = Event.create!(
   start_date: Date.today + 4.weeks,
   end_date: Date.today + 4.weeks,
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  title: "Event",
+  title: "Event Title",
   schedule: "#{(Date.today + 4.weeks).strftime("%A")} 6-8pm",
   event_type: "event"
 )
