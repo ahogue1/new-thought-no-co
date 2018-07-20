02704364661d381a1550806c50c73d150c4aacb8
Amy Hogue <hogue.a@gmail.com>
2018-07-20 11:49:27 -0500

    Change Contact Mailer recipient to info@newthoughtnoco.org

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -7,12 +7,12 @@ class ContactMailer < ApplicationMailer
   #
   def prayer_request(request)
     @request = request
-    mail(to: "hogue.a@gmail.com", subject: "New Prayer Request")
+    mail(to: "info@newthoughtnoco.org", subject: "New Prayer Request")
   end
 
   def event_registration(registration)
     @registration = registration
     @event = registration.event
-    mail(to: "hogue.a@gmail.com", subject: "New #{@event.event_type.capitalize} Registration")
+    mail(to: "info@newthoughtnoco.org", subject: "New #{@event.event_type.capitalize} Registration")
   end
 end
