b68822970e3a96ee8f9c6d51b0ef156bd3cb126a
Amy Hogue <hogue.a@gmail.com>
2018-08-18 15:23:39 -0500

    Fix error when there is no scroll to top button on page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,10 +6,14 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-    document.getElementById("myBtn").style.display = "block";
-  } else {
-    document.getElementById("myBtn").style.display = "none";
+  var myBtn = document.getElementById("myBtn");
+
+  if(myBtn) {
+    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+      myBtn.style.display = "block";
+    } else {
+      myBtn.style.display = "none";
+    }
   }
 }
 
